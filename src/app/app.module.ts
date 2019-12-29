d9ed14408420798fb290dff7459651683c008d4a
Harald Brabenetz <brabenetz@gmx.net>
2019-12-29 13:14:11 +0100

    add simple primeng form to start game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
+import { DropdownModule, Dropdown } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayComponent } from './play/play.component';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
         AppComponent,
-        PlayComponent
+        PlayComponent,
     ],
     imports: [
+        FormsModule,
+        NoopAnimationsModule,
         BrowserModule,
         AppRoutingModule,
         MenubarModule,
+        DropdownModule,
+        ButtonModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api/selectitem';
 
 @Component({
-  selector: 'app-play',
-  templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+    selector: 'app-play',
+    templateUrl: './play.component.html',
+    styleUrls: ['./play.component.scss']
 })
 export class PlayComponent implements OnInit {
 
-  constructor() { }
+    public availablePlayers: SelectItem[];
 
-  ngOnInit() {
-  }
+    public player1 = 'human';
 
+    public player2 = 'human';
+
+    public gameIsRunning = false;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.availablePlayers = [
+            { label: 'Human', value: 'human' },
+            { label: 'Robot - Random', value: 'robot-random' },
+            { label: 'Robot - Minimax', value: 'robot-minimax' },
+            { label: 'Robot - Ki - ???', value: 'robot-ki-??' },
+        ];
+    }
+
+    stopGame(): void {
+        this.gameIsRunning = false;
+    }
+
+    startGame(): void {
+        this.gameIsRunning = true;
+    }
 }
